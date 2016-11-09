53191ce1acc3ca2d955d2e74fcc2844fd30d1d44
Žiga Vukčevič <ziga@be-codified.com>
2016-11-09 10:06:01 +0100

    Moving files into new repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# --- Work in progress ---
+
+# Install
+
+`npm install`
+
+# Run
+
+`npm run serve`
+
+Open browser at `http://localhost:8080`
+
+## Resources
+
+- https://lodash.com/docs/4.16.4#throttle
+- https://css-tricks.com/debouncing-throttling-explained-examples/
---
+++ js/main.js
@@ -0,0 +1,39 @@
+// NOTE: https://www.sitepoint.com/throttle-scroll-events/
+
+import throttle from 'lodash/throttle';
+
+// Init listening for window scroll event
+window.addEventListener('scroll', expensiveFunction);
+
+// Listening for checkbox click event
+const checkbox = document.querySelector('.throttle');
+
+checkbox.addEventListener('click', (event) => {
+    const
+        time = 1000,
+        isEnabled = checkbox.checked;
+
+    if (isEnabled) {
+        window.removeEventListener('scroll', expensiveFunction);
+        window.addEventListener('scroll', throttle(expensiveFunction, time));
+    }
+    else {
+        window.removeEventListener('scroll', throttle(expensiveFunction, time));
+        window.addEventListener('scroll', expensiveFunction);
+    }
+});
+
+function expensiveFunction() {
+    for (let i = 0; i < 1000; i++) {
+        let randomNumber = Math.floor((Math.random() * 10000) + 1);
+        console.log(randomNumber);
+    }
+
+    const numberOfCalls = document.querySelector('.no-of-calls');
+    numberOfCalls.innerText = parseInt(numberOfCalls.innerText) + 1;
+}
+
+// Accept replacement modules
+if (module.hot) {
+    module.hot.accept();
+}
